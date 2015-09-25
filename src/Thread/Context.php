fc56c49574d60b892a8f471432910fff671332c6
gfonseca <gfonseca@bitbucket.org>
2015-09-25 15:36:29 -0300

    Context included

---
+++ src/Thread/Context.php
@@ -0,0 +1,58 @@
+<?php
+namespace AlThread\Thread;
+
+use AlThread\Thread\Exception\ContextException;
+
+class Context
+{
+    private $vars;
+
+    public function addBulk($key, $item)
+    {
+        $cont_key = null;
+        foreach (func_get_args() as $k => $v) {
+            if ($k % 2 == 0) {
+                $cont_key = $v;
+            } else {
+                $this->addItem($v, $cont_key);
+                $cont_key = null;
+            }
+        }
+    }
+
+    public function delItem($key)
+    {
+        if (!$this->checkItemexists($key)) {
+            throw new ContextException("Key $key do not exists.");
+        }
+
+        unset($this->vars[$key]);
+    }
+
+    public function addItem($obj, $key = null)
+    {
+        if ($key == null) {
+            $this->vars[] = $obj;
+        } else {
+            if ($this->checkItemexists($key)) {
+                throw new ContextException("Key $key already in use.");
+            } else {
+                $this->vars[$key] = $obj;
+            }
+        }
+    }
+
+    public function checkItemExists($key)
+    {
+        return isset($this->vars[$key]);
+    }
+
+    public function getItem($key)
+    {
+        if (!$this->checkItemexists($key)) {
+            throw new ContextException("Key $key do not exists.");
+        }
+
+        return $this->vars[$key];
+    }
+}
